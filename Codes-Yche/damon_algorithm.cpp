35e5683441755f5d246617e5362a276b6f8e6cef
cheyulin <cheyulin@localhost.localdomain>
2016-04-10 15:57:08 +0800

    update header to make correct usage of bgl and uniqueptr

--- Codes-Yche/damon_algorithm.cpp
+++ Codes-Yche/damon_algorithm.cpp
@@ -5,41 +5,36 @@
 #include "damon_algorithm.h"
 
 namespace yche {
-    unique_ptr<Daemon::SubGraph> Daemon::ExtractEgoMinusEgo(Daemon::VertexDescriptor ego_vertex) {
+    unique_ptr<Daemon::SubGraph> Daemon::ExtractEgoMinusEgo(Daemon::Vertex ego_vertex) {
         unique_ptr<SubGraph> ego_net_ptr = make_unique<SubGraph>();
-        auto adjacency_iterator_pair = adjacent_vertices(ego_vertex, graph_);
-        auto origin_index_to_sub_index_map = map<VertexIndexType, VertexIndexType>();
+        auto origin_index_to_sub_index_map = map<int, int>();
 
-        auto end = adjacency_iterator_pair.second;
-        int sub_index = 0;
-        property_map<SubGraph, vertex_id_t>::type SubGraphVertexId =
-                get(vertex_id_t(), *ego_net_ptr);
-        property_map<SubGraph, vertex_weight_t>::type SubGraphVertexWeight =
-                get(vertex_weight_t(), *ego_net_ptr);
-        property_map<SubGraph, vertex_label_t>::type SubGraphVertexLabel =
-                get(vertex_label_t(), *ego_net_ptr);
-        property_map<Graph, vertex_index_t>::type GraphVertexIndex =
-                get(vertex_index_t(), graph_);
-        property_map<Graph, vertex_weight_t>::type GraphVertexWeight =
-                get(vertex_weight_t(), graph_);
+        property_map<SubGraph, vertex_id_t >::type sub_vertex_id_map =
+                get(vertex_id, *ego_net_ptr);
+        property_map<SubGraph, vertex_weight_t>::type sub_vertex_weight_map =
+                get(vertex_weight, *ego_net_ptr);
+        property_map<SubGraph, vertex_label_t>::type sub_vertex_label_map =
+                get(vertex_label, *ego_net_ptr);
+        property_map<Graph, vertex_index_t>::type vertex_index_map =
+                get(vertex_index, *graph_ptr_);
+        property_map<Graph, vertex_weight_t>::type vertex_weight_map =
+                get(vertex_weight, *graph_ptr_);
 
-        for (auto begin = adjacency_iterator_pair.first; begin != end; ++begin) {
-            //Add VertexDescriptor
-            SubGraphVertexDescriptor vertex = add_vertex(*ego_net_ptr);
+        for (auto vp = adjacent_vertices(ego_vertex, *graph_ptr_); vp.first != vp.second; ++vp.first) {
+            //Add Vertex
+            graph_traits<SubGraph>::vertex_descriptor sub_vertex = add_vertex(*ego_net_ptr);
 
-            //Add VertexDescriptor Property
-            VertexIndexType graph_vertex_index = get(GraphVertexIndex, *begin);
-            put(SubGraphVertexId, vertex, graph_vertex_index);
-            put(SubGraphVertexWeight, vertex, get(GraphVertexWeight, *begin));
-
-            array<VertexIndexType, 2> label_array;
-            label_array[0] = graph_vertex_index;
+            //Add Vertex Property
+            auto graph_vertex_index = vertex_index_map[*vp.first];
+            sub_vertex_id_map[sub_vertex] = graph_vertex_index;
+            sub_vertex_weight_map[sub_vertex] = vertex_weight_map[*vp.first];
+            array<int, 2> label_array;
+            label_array[0] = sub_vertex_id_map[sub_vertex];
             label_array[1] = 0;
-            put(SubGraphVertexLabel, vertex, label_array);
-
+            sub_vertex_label_map[sub_vertex] = label_array;
             //Add Indexing Info
+
             origin_index_to_sub_index_map.insert(make_pair(graph_vertex_index, sub_index));
-            sub_index++;
         }
 
         //Add Edges
@@ -61,12 +56,12 @@ namespace yche {
     }
 
     Daemon::OverlappingCommunityVec Daemon::DetectCommunitesViaLabelPropagation
-            (unique_ptr<Daemon::Graph> sub_graph, Daemon::SubGraphVertexDescriptor ego_vertex) {
+            (unique_ptr<Daemon::Graph> sub_graph, Daemon::SubGraphVertex ego_vertex) {
         int iteration_num = 0;
         auto vertices = sub_graph->m_vertices;
 
-        property_map<SubGraph, vertex_id_t>::type SubGraphVertexId =
-                get(vertex_id_t(), *sub_graph);
+        property_map<SubGraph, vertex_index_t>::type SubGraphVertexId =
+                get(vertex_index_t(), *sub_graph);
         property_map<SubGraph, vertex_weight_t>::type SubGraphVertexWeight =
                 get(vertex_weight_t(), *sub_graph);
         property_map<SubGraph, vertex_label_t>::type SubGraphVertexLabel =
@@ -80,7 +75,7 @@ namespace yche {
                 auto adjacency_iterator_pair = adjacent_vertices(vertex_descriptor, graph_);
                 auto end = adjacency_iterator_pair.second;
 
-                auto label_to_weight_map = map<VertexIndexType, double>();
+                auto label_to_weight_map = map<int, double>();
                 //Label Propagation
                 for (auto begin = adjacency_iterator_pair.first; begin != end; ++begin) {
                     auto neighbor_vertex_label = get(SubGraphVertexLabel, *begin)[last_index_indicator];
@@ -95,7 +90,7 @@ namespace yche {
                 }
                 //Find Maximum Vote
 
-                auto candidate_label_vec = vector<VertexIndexType>();
+                auto candidate_label_vec = vector<int>();
                 auto max_val = 0;
                 for (auto &label_to_weight_pair:label_to_weight_map) {
                     auto label_weight = label_to_weight_pair.second;
--- Codes-Yche/damon_algorithm.h
+++ Codes-Yche/damon_algorithm.h
@@ -12,59 +12,62 @@
 using namespace boost;
 using namespace std;
 
+//Add User-Defined Tags
+enum vertex_weight_t {
+    vertex_weight;
+};
+enum vertex_label_t {
+    vertex_label;
+};
+
+enum vertex_id_t{
+    vertex_id;
+};
+
+namespace boost {
+    BOOST_INSTALL_PROPERTY(vertex, weight);
+    BOOST_INSTALL_PROPERTY(vertex, label);
+    BOOST_INSTALL_PROPERTY(vertex, id);
+}
+
 namespace yche {
-    using VertexIndexType = int;
-    //Property Tag
-    struct  SubGraphVertexProperties{
-        typedef vertex_property_type type;
-    };
-    struct vertex_id_t{
-        typedef vertex_property_type type;
-    };
-    struct vertex_weight_t{
-        typedef vertex_property_type type;
-    };
-    struct vertex_label_t{
-        typedef vertex_property_type type;
-    };
-
     class Daemon {
-        using Graph = adjacency_list<vecS, setS, undirectedS,property<vertex_weight_t,double >>;
-        using VertexDescriptor = graph_traits<Graph>::vertex_descriptor;
+        using VertexProperties = property<vertex_weight_t, double,
+                property<vertex_index_t, int>>;
+        using Graph = adjacency_list<vecS, setS, undirectedS, VertexProperties>;
 
-        using GraphVertexProperty= property<vertex_weight_t, double>;
+        using SubGraphVertexProperties = property<vertex_weight_t, double,
+                property<vertex_id_t , int>,
+                property<vertex_label_t, array<int, 2>>>;
 
-        using SubGraphVertexProperty = property<vertex_id_t, VertexIndexType,
-                property<vertex_weight_t, double,
-                        property<vertex_label_t, array<VertexIndexType, 2>>>>;
-        using SubGraph = adjacency_list<vecS, vecS, undirectedS, SubGraphVertexProperty>;
-        using SubGraphVertexDescriptor = graph_traits<SubGraph>::vertex_descriptor;
+        using SubGraph = adjacency_list<vecS, vecS, undirectedS, SubGraphVertexProperties>;
 
-        using Community = unique_ptr<set<VertexIndexType>>;
+        using Vertex = graph_traits<Graph>::vertex_descriptor;
+        using SubGraphVertex = graph_traits<SubGraph>::vertex_descriptor;
+        using Community = unique_ptr<set<int>>;
         using OverlappingCommunityVec = vector<Community>;
 
     private:
-        Graph graph_;
+        unique_ptr<Graph> graph_ptr_;
         OverlappingCommunityVec overlapping_community_vec_;
         double epsilon_;
         int min_community_size_;
         int max_iteration_num_;
 
-        unique_ptr<SubGraph> ExtractEgoMinusEgo(VertexDescriptor ego_vertex);
+        unique_ptr<SubGraph> ExtractEgoMinusEgo(Vertex ego_vertex);
 
         OverlappingCommunityVec DetectCommunitesViaLabelPropagation(
-                unique_ptr<Graph> sub_graph, SubGraphVertexDescriptor ego_vertex);
+                unique_ptr<Graph> sub_graph, SubGraphVertex ego_vertex);
 
         void MergeTwoCommunities(Community left_community, Community right_community);
 
         void MergeIntoOverallCommunities(OverlappingCommunityVec &new_created_communities_vec);
 
     public:
-        Daemon(double epsilon, int min_community_size, vector<pair<int, int>> &edge_list,
-               long vertices_size, int max_iteration) :
+        Daemon(double epsilon, int min_community_size, unique_ptr<Graph> graph_ptr, int max_iteration) :
                 epsilon_(epsilon), min_community_size_(min_community_size), max_iteration_num_(max_iteration) {
-            graph_ = Graph(edge_list.begin(), edge_list.end(), vertices_size);
-
+;                std::move(graph_ptr);
+            graph_ptr_=std::move(graph_ptr);
         }
 
         void ExecuteDaemon();
