7ec68521a26c433e8bed876370e1609b719233d0
cheyulin <cheyulin@csz110.mobilenet.cse.ust.hk>
2016-04-14 14:01:07 +0800

    update cis to compile it in c++11

--- Overlapping-Community-Detection/2009-Connected-Iterative-Scan/Libraries/Params/Parameters.h
+++ Overlapping-Community-Detection/2009-Connected-Iterative-Scan/Libraries/Params/Parameters.h
@@ -7,6 +7,7 @@
 
 #ifndef ICE_PARAM
 #define ICE_PARAM
+
 #include <vector>
 #include <string>
 #include <map>
@@ -24,78 +25,76 @@ typedef long unsigned int lui;
  * 
  * Structure to help make reading command line arguments quick and easy
  */
-class Parameters{
-	public:
-	  /** @fn Parameters()
-	   * 
-	   * Empty Constructor
-	   */
-		Parameters(){
-			
-		}
-		
-	    /** @fn ~Parameters()
-	   * 
-	   * Empty Destructor
-	   */
-		~Parameters(){
-			
-		}
-		
-		void Read(int argc, char** argv);
-		
-		/**
-		 * @fn template <class T> void set (T* var, string tag, T dflt)
-		 * @param var Pointer to variable to set to parameter
-		 * @param tag Command line flag to look for
-		 * @param dflt Default value to set var to
-		 */
-		template <class T>
-		void set (T* var, string tag, T dflt){
-			if ((it_lookup = lookup.find(tag)) != lookup.end())
-			{
-			  (*var) = str_to<T>(it_lookup->second[0]);
-			}
-			else
-			  (*var) = dflt;			
-		}
-		
-		/**
-		 * @fn template <class T> void set (T* var, string tag)
-		 * @param var Pointer to variable to set to parameter
-		 * @param tag Command line flag to look for
-		 * 
-		 * Will throw an exception if the flag is not found in the parameters
-		 */
-		template <class T>
-		void set (T* var, string tag){
-			if ((it_lookup = lookup.find(tag)) != lookup.end())
-			{
-			  (*var) = str_to<T>(it_lookup->second[0]);
-			}
-			else	
-			  throw "Invalid Parameters";
-		}
-		
-		/**
-		 * @fn void boolset (bool* var, string tag)
-		 * @param var Pointer to variable to set to parameter
-		 * @param tag Command line flag to look for
-		 * 
-		 * Will set the boolean to true if the flag is found. False otherwise.
-		 */
-		void boolset (bool* var, string tag){
-		  if ((it_lookup = lookup.find(tag)) != lookup.end())
-			{
-			  (*var) = true;
-			}
-			else{
-			  (*var) = false;
-			}
-		}
-		
-	private:
-		map <string, vector < string > > lookup;
-		map <string, vector < string > >::iterator it_lookup;
+class Parameters {
+public:
+    /** @fn Parameters()
+     *
+     * Empty Constructor
+     */
+    Parameters() {
+
+    }
+
+    /** @fn ~Parameters()
+   *
+   * Empty Destructor
+   */
+    ~Parameters() {
+
+    }
+
+    void Read(int argc, char **argv);
+
+    /**
+     * @fn template <class T> void set (T* var, string tag, T dflt)
+     * @param var Pointer to variable to set to parameter
+     * @param tag Command line flag to look for
+     * @param dflt Default value to set var to
+     */
+    template<class T>
+    void set(T *var, string tag, T dflt) {
+        if ((it_lookup = lookup.find(tag)) != lookup.end()) {
+            (*var) = str_to<T>(it_lookup->second[0]);
+        }
+        else
+            (*var) = dflt;
+    }
+
+    /**
+     * @fn template <class T> void set (T* var, string tag)
+     * @param var Pointer to variable to set to parameter
+     * @param tag Command line flag to look for
+     *
+     * Will throw an exception if the flag is not found in the parameters
+     */
+    template<class T>
+    void set(T *var, string tag) {
+        if ((it_lookup = lookup.find(tag)) != lookup.end()) {
+            (*var) = str_to<T>(it_lookup->second[0]);
+        }
+        else
+            throw "Invalid Parameters";
+    }
+
+    /**
+     * @fn void boolset (bool* var, string tag)
+     * @param var Pointer to variable to set to parameter
+     * @param tag Command line flag to look for
+     *
+     * Will set the boolean to true if the flag is found. False otherwise.
+     */
+    void boolset(bool *var, string tag) {
+        if ((it_lookup = lookup.find(tag)) != lookup.end()) {
+            (*var) = true;
+        }
+        else {
+            (*var) = false;
+        }
+    }
+
+private:
+    map <string, vector<string>> lookup;
+    map <string, vector<string>>::iterator it_lookup;
 };
+
 #endif
