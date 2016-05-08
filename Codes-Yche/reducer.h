c30fbffb011fcd5aae33e203b1c0b85981dbe7cc
cheyulin <yche@cse.ust.hk>
2016-05-08 11:33:10 +0800

    add template typename def

--- Codes-Yche/reducer.h
+++ Codes-Yche/reducer.h
@@ -10,6 +10,7 @@
 namespace yche {
     using namespace std;
 
+    template<typename DataCollection, typename Data, typename DataCmpFunction, typename ComputationFunction>
     class Reducer {
     private:
         unsigned long thread_count_;
@@ -17,17 +18,62 @@ namespace yche {
         unsigned long barrier_count_;
 
         pthread_t *thread_handles;
-        vector<sem_t> sem_mail_boxes_;
 
+        vector<unique_ptr<Data>> local_data_vec_;
+        vector<sem_t> sem_mail_boxes_;
         vector<bool> is_rec_mail_empty_;
+
+        DataCmpFunction data_cmp_function_;
+        ComputationFunction compute_fucntion_;
 
         void LoopCommThreadFunction(unsigned long thread_id);
 
         static void *InvokeLoopCommThreadFunction(void *bundle_input_ptr);
 
-        void InitDataPerThread();6
+        void InitDataPerThread();
 
+    public:
+
+        void ParallelExecute();
+
+        Reducer(unsigned long thread_count_, DataCollection reduce_data, DataCmpFunction data_cmp_function_,
+                ComputationFunction compute_fucntion_)
+                : thread_count_(thread_count_), data_cmp_function_(data_cmp_function_),
+                  compute_fucntion_(compute_fucntion_) {
+            thread_handles = new pthread_t[thread_count_];
+            sem_mail_boxes_.resize(thread_count_);
+            for (auto i = 0; i < thread_count_; ++i) {
+                sem_init(&sem_mail_boxes_[i], NULL, 0);
+            }
+
+        }
+
+        virtual ~Reducer() {
+            for (auto i = 0; i < thread_count_; ++i) {
+                sem_destroy(&sem_mail_boxes_[i]);
+            }
+            delete[]thread_handles;
+        }
     };
+
+
+    void Reducer::InitDataPerThread() {
+
+    }
+
+    void Reducer::LoopCommThreadFunction(unsigned long thread_id) {
+
+    }
+
+    void *Reducer::InvokeLoopCommThreadFunction(void *bundle_input_ptr) {
+        return nullptr;
+    }
+
+    void Reducer::ParallelExecute() {
+        //InitLocalData
+        InitDataPerThread();
+    }
+
 
 }
 
