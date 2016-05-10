1892738a4f9c3e6d1003736dbf91b0bcafbc0773
cheyulin <yche@cse.ust.hk>
2016-05-10 22:40:15 +0800

    add new auto-execute exp shell

---
+++ Codes-Yche/execute_exp.sh
@@ -0,0 +1,7 @@
+source ./run_cis_demon.sh
+echo 'start'
+run_with_different_thread_count demo_input_files/collaboration_edges_input.csv demo_output_files/collaboration_  seq
+run_with_different_thread_count demo_input_files/collaboration_edges_input.csv demo_output_files/collaboration_with_reduce_  reduce
+run_with_different_thread_count ../Dataset/social_network/facebook_combined.txt demo_output_files/facebook_  seq
+run_with_different_thread_count ../Dataset/social_network/facebook_combined.txt demo_output_files/facebook_with_reduce_  reduce
+echo 'end'
---
+++ Codes-Yche/execute_exp_draw.sh
@@ -0,0 +1,5 @@
+cd util/
+python get_statistics.py ../demo_output_files/  collaboration
+python get_statistics.py ../demo_output_files/  collaboration_with_reduce
+python get_statistics.py ../demo_output_files/  facebook
+python get_statistics.py ../demo_output_files/  facebook_with_reduce
