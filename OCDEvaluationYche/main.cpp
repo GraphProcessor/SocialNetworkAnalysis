c77db1dce15b90df6ed2aba89c39039bc5b48f84
cheyulin <yche@cse.ust.hk>
2016-05-17 20:01:54 +0800

    not figure out nomalization

--- OCDEvaluationYche/main.cpp
+++ OCDEvaluationYche/main.cpp
@@ -5,10 +5,10 @@ using namespace std;
 int main(int argc, char *argv[]) {
     char *input_str = argv[1];
     char *output_str = argv[2];
-    auto calculation = [](double left, double right) -> double {
+//    auto calculation = [](double left, double right) -> double {
 //        cout <<"Calculate Result:"<<1.0 / ((1 + exp(60 - 60 * left)) * ((1 + exp(60 - 60 * right))))<<endl;
-        return 1.0 / ((1 + exp(2 - 1 * left)) * ((1 + exp(2 - 1 * right))));
-    };
+//        return 1.0 / ((1 + exp(2 - 1 * left)) * ((1 + exp(2 - 1 * right))));
+//    };
 
 //    auto calculation = [](double left, double right) -> double {
 //        return left * right;
@@ -18,6 +18,9 @@ int main(int argc, char *argv[]) {
 //        return std::min(left, right);
 //    };
 
+        auto calculation = [](double left, double right) -> double {
+        return std::max(left, right);
+    };
     using namespace yche;
     ModularityLinkBelonging<decltype(calculation)> modularity_calc(input_str, output_str, calculation);
     cout << modularity_calc.CalculateModularity();
