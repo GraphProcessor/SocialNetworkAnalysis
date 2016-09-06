f01ae549a76cb109a99eae75b9d2b67679241f29
Lipeng WANG <wang.lp@outlook.com>
2016-09-06 14:43:09 +0800

    fix addTask bug

--- Codes-Yche/src/parallel_utils/thread_pool_breakable.h
+++ Codes-Yche/src/parallel_utils/thread_pool_breakable.h
@@ -25,7 +25,7 @@ namespace yche {
 
     class ThreadPoolBreakable : public ThreadPoolBase<BreakWithCallBackRetType> {
     private:
-        bool is_break_{false};
+        atomic_bool is_break_{false};
         mutex call_back_mutex_;
 
     protected:
@@ -44,8 +44,8 @@ namespace yche {
                         auto lock = make_unique_lock(call_back_mutex_);
                         if (!is_break_) {
                             auto lock = make_unique_lock(task_queue_mutex_);
-                            left_tasks_counter_ = 1;
                             is_break_ = true;
+                            left_tasks_counter_ = 1;
                             task_queue_.clear();
                             call_back_ret_obj.call_back_function_object_();
                         }
@@ -63,8 +63,13 @@ namespace yche {
         ThreadPoolBreakable(int thread_count) : ThreadPoolBase(thread_count) {}
 
         virtual void AddTask(std::function<BreakWithCallBackRetType()> task) override {
-            if (!is_break_)
-                ThreadPoolBase::AddTask(task);
+            auto lock = make_unique_lock(task_queue_mutex_);
+            if (!is_break_) {
+                task_queue_.emplace_back(std::move(task));
+
+                ++left_tasks_counter_;
+                task_available_cond_var_.notify_one();
+            }
         }
 
         void WaitForBreakOrTerminate(bool &is_break) {
